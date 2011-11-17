d325bef629b67d5d31ec4f1440f019eebbb0235a
Rich Grundy <rich@stinkyink.com>
2011-11-17 13:43:19 +0000

    Initial commit

---
+++ lib/star-rating-rails.rb
@@ -0,0 +1,9 @@
+require "star-rating-rails/version"
+
+module Star
+  module Rating
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/star-rating-rails/version.rb
@@ -0,0 +1,7 @@
+module Star
+  module Rating
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
