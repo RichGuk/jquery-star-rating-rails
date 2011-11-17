8107fff66db0af39dcdee0552c8dc283b79354ac
Rich Grundy <rich@stinkyink.com>
2011-11-17 13:56:50 +0000

    Added engine file.

--- lib/star-rating-rails.rb
+++ lib/star-rating-rails.rb
@@ -1,9 +1,2 @@
 require "star-rating-rails/version"
-
-module Star
-  module Rating
-    module Rails
-      # Your code goes here...
-    end
-  end
-end
+require "star-rating-rails/engine"
---
+++ lib/star-rating-rails/engine.rb
@@ -0,0 +1,8 @@
+module Star
+  module Rating
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
